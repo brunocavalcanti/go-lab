ed4ff927672937581e11dafc887268931113bf5b
Bruno Cavalcanti <bh-cavalcantii@hotmail.com>
2018-11-29 22:27:37 -0200

    custom error

--- project-1/main.go
+++ project-1/main.go
@@ -10,17 +10,21 @@ import (
 )
 
 type Todo struct {
-	ID          uuid.UUID
-	Description string
+	ID          uuid.UUID `json:"uuid,omitempty"`
+	Description string    `json:"description,omitempty"`
 }
 type TodoCreation struct {
 	Description string `json:"description,omitempty"`
+}
+type ErrorHandlerApi struct {
+	Message string `json:"message,omitempty"`
 }
 
 var todos = []Todo{}
 
 func main() {
 	router := mux.NewRouter()
+	router.Use(jsonMiddleware)
 	router.HandleFunc("/todo", getAll).Methods("GET")
 	router.HandleFunc("/todo/{id}", get).Methods("GET")
 	router.HandleFunc("/todo", create).Methods("POST")
@@ -30,27 +34,53 @@ func main() {
 }
 
 func getAll(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(todos)
+	onSucces(w, todos, http.StatusOK)
 }
 func get(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	uuidTodo := uuid.FromStringOrNil(vars["id"])
 
-	for _, todo := range todos {
-		if todo.ID == uuidTodo {
-			json.NewEncoder(w).Encode(&todo)
-			break
+	uuidTodo, err := uuid.FromString(vars["id"])
+
+	if err != nil {
+		onError(w, "Invalid Invalid!", http.StatusBadRequest)
+		return
+	}
+	for _, item := range todos {
+		if item.ID == uuidTodo {
+			onSucces(w, item, http.StatusOK)
+			return
 		}
 	}
-	json.NewEncoder(w).Encode(Todo{})
-
+	onError(w, "Todo not found!", http.StatusNotFound)
 }
 func create(w http.ResponseWriter, r *http.Request) {
 	var data TodoCreation
-	_ = json.NewDecoder(r.Body).Decode(&data)
+	err := json.NewDecoder(r.Body).Decode(&data)
+
+	if err != nil {
+		onError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
 	id, _ := uuid.NewV4()
 	newTodo := Todo{Description: data.Description, ID: id}
 	todos = append(todos, newTodo)
-	json.NewEncoder(w).Encode(data)
 
+	onSucces(w, newTodo, http.StatusCreated)
+}
+func onError(w http.ResponseWriter, message string, status int) {
+	w.WriteHeader(status)
+	err := ErrorHandlerApi{message}
+	json.NewEncoder(w).Encode(err)
+}
+func onSucces(w http.ResponseWriter, ret interface{}, status int) {
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(ret)
+}
+
+func jsonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
